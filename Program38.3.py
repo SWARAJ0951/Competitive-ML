d68b29259b78390145e4e4a2c1d19b474fd5a20b
Swaraj Jagtap <swarajjagtap22@gmail.com>
2026-08-14 21:07:18 +0530

    Add files via upload

---
+++ Program38.3.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def main():
+    datapath = "student_performance_ml.csv"
+
+    df = pd.read_csv(datapath)
+
+    print ("First 5 Records are :",df.head(5))       #first 5 records
+    print ("Last 5 Records are :" ,df.tail(5))       #last 5 records
+    print ("Total Number of rows and colums are:",df.shape)
+    print("Column names :",list(df.columns))
+    print (df.dtypes)
+    print("Total Number of Students are :",df.shape[0])
+
+    matching_rows = (df["FinalResult"]== 1).sum()
+    print("Studnets Passed :",matching_rows)
+
+    matching_rows = (df["FinalResult"]== 0).sum()
+    print("Studnets Failed :",matching_rows)
+
+    avearge = df["StudyHours"].mean()
+    print("Average Study Hours ",avearge) 
+
+    avearge = df["Attendance"].mean()
+    print("Average Attendance ",avearge)  
+
+    maximum = df["PreviousScore"].max()
+    print("Maximum Previous Score :",maximum)
+
+    minimum = df["SleepHours"].min()
+    print("Minimum SleepHours :",minimum)
+
+     
+
+if __name__ == "__main__":
+    main()
